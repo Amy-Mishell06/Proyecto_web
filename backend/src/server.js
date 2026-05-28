9e2b2a4b0d641172d8c01bfd018549e13ce13466
michael-epn <michael.vargas@epn.edu.ec>
2026-05-27 21:11:16 -0500

    sin port

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,11 +11,6 @@ dotenv.config()
 app.use(express.json())
 app.use(cors())
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
-
 app.get('/', (req, res) => {
     res.send("Sistema Inteligente de Recomendacion de Tesis")
 })
